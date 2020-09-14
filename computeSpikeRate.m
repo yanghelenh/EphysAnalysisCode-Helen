77e72874d87536b2bba7678362e4b2cd29a52630
yanghelenh <yang.helenh@gmail.com>
2020-09-14 01:52:46 -0400

    change spike detection to only report spike starts, not spike peak and end too

--- computeSpikeRate.m
+++ computeSpikeRate.m
@@ -16,6 +16,27 @@
 % UPDATED:
 %   9/14/20 - HHY
 %
-function spikeRate = computeSpikeRate(spikeInd, t, window)
+function spikeRate = computeSpikeRate(spikeInd, t, winSize)
 
+    % find inter-sample interval (i.e. inverse of sample rate), in seconds
+    isi = median(diff(t));
+    
+    % get size of window, in samples
+    winSizeSamp = round(winSize / isi);
+    winSizeSec = winSizeSamp * isi; % window size in seconds, post rounding
+    
+    % vector of size t where 0 for no spike, 1 for spike
+    spikeLog = zeros(size(t));
+    spikeLog(spikeInd) = 1;
+    
+    % compute moving average over spike logical vector, using specified
+    %  window size; divide by window size in seconds to get spike rate
+    spikeRate = movmean(spikeLog, winSizeSamp);
+    
+    
+    spikeTimes = t(spikeInd);
+    intSpikeInt = diff(spikeTimes);
+    
+    spikeRate = ones(size(intSpikeInt))./intSpikeInt;
+    
 end
--- detectSpikes.m
+++ detectSpikes.m
@@ -5,11 +5,7 @@
 %  first derivative of voltage (dv/dt). Adacent threshold crossings must be
 %  at least refractoryPeriod length of time from each other.
 % 
-% Returns indicies of peak, start, and end. Start and end defined by
-%  specified thresholds. Peak is max between them.
-% 
-% If start and end vectors aren't the same length (error in calling
-%  spikes), peak indicies are not returned.
+% Returns indicies of spike start.
 %
 % When two threshold crossings are too close together, assumes the latter
 %  is wrong.
@@ -17,24 +13,21 @@
 % INPUTS:
 %   voltage - voltage trace
 %   t - time trace
-%   dvdtThresh - struct for setting thresholds on first derivative for
-%           spike detection
-%       start - threshold to cross from below for start of spike
-%       end - threshold to cross from below for at end of spike
+%   dvdtThresh - threshold to cross from below for start of spike
 %   refractoryPeriod - length of time in seconds where another spike can't
 %       occur
 %
 % OUTPUTS:
-%   peakInd - indicies for peak/max of each action potential
 %   startInd - indicies for start of each action potential
 %
 % CREATED: 9/13/20 - HHY
 %
 % UPDATED:
 %   9/13/20 - HHY
+%   9/14/20 - HHY - change to only report spike start indicies, instead of
+%       start, end, and peak (auto correction too hard for peak)
 %
-function [peakInd, startInd, endInd] = detectSpikes(voltage, t, ...
-    dvdtThresh, refractoryPeriod)
+function startInd = detectSpikes(voltage, t, dvdtThresh, refractoryPeriod)
 
     % find inter-sample interval (i.e. inverse of sample rate), in seconds
     isi = median(diff(t));
@@ -46,74 +39,19 @@ function [peakInd, startInd, endInd] = detectSpikes(voltage, t, ...
     dvdt = diff(voltage) ./ diff(t);
     
     % find when first derivative of voltage exceeds threshold
-    abvThresh = dvdt > dvdtThresh.start;
-    blwThresh = dvdt < dvdtThresh.end;
+    abvThresh = dvdt > dvdtThresh;
     
 	% find indicies for when these threshold crossings occur
     spikeStartInd = find(diff(abvThresh) == 1); % 1st to exceed thresh
-    spikeEndInd = find(diff(blwThresh) == -1); % last to be below thresh
     
     % correct for taking difference
     spikeStartInd = spikeStartInd + 1;
-    spikeEndInd = spikeEndInd + 1;
     
     % correct for if there are threshold crossings too close together
     %  (within refractory period)
     spikeStartIndRefrCorr = rmRefrPrdThreshCross(spikeStartInd, ...
         refrPrdSamp);
-    spikeEndIndRefrCorr = rmRefrPrdThreshCross(spikeEndInd, refrPrdSamp);
     
-    % first spike end index must not be prior to first spike start index
-    %  delete first index if that's the case
-    if (spikeEndIndRefrCorr(1) < spikeStartIndRefrCorr(1))
-        spikeEndIndRefrCorr(1) = [];
-    end
-    
-    % last spike start ind must not be later than last spike end ind; if it
-    %  is, delete last element in spike start ind array
-    if (spikeStartIndRefrCorr(end) > spikeEndIndRefrCorr(end))
-        spikeStartIndRefrCorr(end) = [];
-    end
-    
-    % 
-    
-    % length of spikeStart and spikeEnd vectors should be same at this
-    %  point; if not, bigger error than can be corrected here; peakInd = []
-    if (length(spikeStartIndRefrCorr) ~= length(spikeEndIndRefrCorr))
-        peakInd = [];
-    else % otherwise, find peaks
-        % find peak of spike as max between the start and end indicies for
-        %  each spike
-        peakInd = zeros(size(spikeStartIndRefrCorr)); % preallocate
-        for i = 1:length(spikeStartIndRefrCorr)
-            % voltage snippet
-            spikeVoltage = voltage(...
-                spikeStartIndRefrCorr(i):spikeEndIndRefrCorr(i));
-            [~, localPeakInd] = max(spikeVoltage);
-            peakInd(i) = localPeakInd + spikeStartIndRefrCorr(i) - 1;
-        end
-    end
-    
-    % outputs
+    % output
     startInd = spikeStartIndRefrCorr;
-    endInd = spikeEndIndRefrCorr;
-    
-    
-%     % test plotting: dots at spike start, peak, end
-%     spikeStartTimes = t(startInd);
-%     spikeEndTimes = t(endInd);
-%     spikeStartVals = voltage(startInd);
-%     spikeEndVals = voltage(endInd);
-%     spikePeakTimes = t(peakInd);
-%     spikePeakVals = voltage(peakInd);
-%     
-%     figure; 
-%     plot(t, voltage);
-%     hold on;
-%     plot(spikeStartTimes, spikeStartVals, 'LineStyle', 'none', ...
-%         'Marker', '.');
-%     plot(spikeEndTimes, spikeEndVals, 'LineStyle', 'none', 'Marker', ...
-%         '.', 'MarkerEdgeColor', 'magenta');
-%     plot(spikePeakTimes, spikePeakVals, 'LineStyle', 'none', 'Marker', ...
-%         '.', 'MarkerEdgeColor', 'green');
 end
